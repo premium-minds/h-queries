4288bb1b78d8916e16d3b6997d5b7587fc634cc0
Rui Barreiro <rui.barreiro@gmail.com>
2014-01-08 16:31:02 +0000

    gadts and parse results

--- Queries.hs
+++ Queries.hs
@@ -1,87 +1,94 @@
-{-# LANGUAGE FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE GADTs, RankNTypes #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
 
 module Queries (
-    toAST
-    , dbMap
-    , queryToSQL
-    , queryEntity
-    , ASTNode(..)
-    , DBWrapper(..)
-    , Entity(..)
+    Query(..)
+    ,toQuery
+    ,hQuery
+    ,createSqliteBackend
+    ,getBackendCode
+    ,qmap
+    ,queryEntity
+    --, QInt
+    ,Entity(..)
 ) where 
     import Data.List
-
+    import qualified Database.HDBC.Sqlite3 as Sqlite
+    import Database.HDBC
+    import qualified Data.ByteString.Char8 as UTF8
+    import qualified Data.ByteString as BS
     
-    data ASTNode = ASTIntLit Integer
-             | Lambda ASTNode
-             | ASTVar
-             | ASTPlus ASTNode ASTNode
-             | GetEntity String
-             | DbMap ASTNode ASTNode
-        deriving Show
 
-    data DBWrapper a = JustValue a | Var | Code ASTNode
+    data Query a where
+        ASTIntLit :: Integer -> Query Integer
+        ASTListLit :: forall b. QType b => [b] -> Query [b]
+        ASTQMap :: (Query a -> Query b) -> Query [a] -> Query [b]
+        ASTVar :: Query b
+        ASTPlusInt :: Query Integer -> Query Integer -> Query Integer
+        ASTGetEntity :: Entity b -> Query b
 
-
-    class DBType a where
-        toAST:: a -> ASTNode
-        makeVar :: (a -> b) -> a
-
-    class DBLit a where
-        toLit :: a -> ASTNode 
-
-
-
-    instance DBLit Integer where
-        toLit x = ASTIntLit x
-
-    instance DBLit a  => DBType (DBWrapper a) where
-        toAST (JustValue x) = toLit x
-        toAST (Var) = ASTVar
-        toAST (Code x) = x
-        makeVar f = Var
-
-    instance (DBType a, DBType b) => DBType (a->b) where
-        toAST f =
-            let 
-                x = makeVar f 
-            in
-                Lambda (toAST $ f x)
-
-
-    instance (DBType (DBWrapper a)) => Num (DBWrapper a) where
-        (+) x y = Code $ ASTPlus (toAST x) (toAST y)
-
-
-
-    data Entity a = Entity String
-    data Query a = Query ASTNode
+    data Entity a = Entity String deriving (Show)
 
     queryEntity :: Entity a -> Query a
-    queryEntity (Entity refName) = Query (GetEntity refName)
-
-    dbMap :: (DBType a, DBType b) => (a->b) -> (Query a) -> (Query b)
-    dbMap f  (Query node) = Query (DbMap (toAST f) node)
+    queryEntity e = ASTGetEntity e
 
     queryToSQL :: Query a -> String
-    queryToSQL (Query node) = queryToSQL' node
+    queryToSQL (ASTIntLit i) = "select " ++ show i
+    queryToSQL (ASTPlusInt x y) = "select (" ++ queryToSQL x ++ ")+(" ++ queryToSQL y ++ ")"
+    queryToSQL ASTVar = "x1"
+    queryToSQL (ASTGetEntity (Entity ref)) = "select * from " ++ ref 
+    queryToSQL (ASTQMap f lx) = "select (" ++ (queryToSQL $ f ASTVar) ++ ") from (" ++ queryToSQL lx ++ ")"
+
+    data QueryRawRes = QueryRawResSimple [BS.ByteString] deriving Show
+
+    class QType a where
+        toQuery :: a -> (Query a)
+        parseQueryRes :: QueryRawRes -> (a, QueryRawRes)
+
+    instance QType Integer where
+        toQuery i = ASTIntLit i
+        parseQueryRes (QueryRawResSimple (bs:r)) = (read $ UTF8.unpack bs, QueryRawResSimple r)
+
+    instance QType a => QType [a] where
+        toQuery l = ASTListLit l 
+        parseQueryRes qr = (collectListRes parseQueryRes qr, QueryRawResSimple [])
+
+
+    collectListRes :: (QueryRawRes -> (a, QueryRawRes)) -> QueryRawRes -> [a]
+    collectListRes f (QueryRawResSimple []) = []
+    collectListRes f l = let (t, r) = f l in [t] ++ collectListRes f r
+
+
+
+    qmap :: (Query a -> Query b) -> Query [a] -> Query [b]
+    qmap f lx = ASTQMap f lx
+
+    instance Num (Query Integer) where
+        x + y = ASTPlusInt x y
+
+    class QBackend a where
+        hQuery :: QType b => a -> Query b -> IO b
+        getBackendCode :: QType b => a -> Query b -> String
+
     
-    queryToSQL' :: (ASTNode) -> String
-    queryToSQL' (GetEntity refName) = "select * from " ++ refName
-    queryToSQL' (ASTIntLit x) = show x
-    queryToSQL' (ASTPlus x y) = queryToSQL' x ++ " + " ++ queryToSQL' y
-    queryToSQL' (ASTVar)      = "x"
-    queryToSQL' (DbMap (Lambda lNode) jNode) = "select " ++ queryToSQL' lNode ++ " from (" ++ queryToSQL' jNode ++ ") as tretas"
-    queryToSQL' x = error $ "Cannot compile " ++ show x ++ "to sql"
+    data SqliteBackend = SqliteBackend Sqlite.Connection
 
+    createSqliteBackend :: FilePath -> IO SqliteBackend
+    createSqliteBackend s = SqliteBackend `fmap` Sqlite.connectSqlite3 s
 
-        
+    sqlValue2QueryRawRes :: [[SqlValue]] -> QueryRawRes
+    sqlValue2QueryRawRes li =
+        let
+            l = concat li
+            trans (SqlByteString bs) = bs
+        in 
+            QueryRawResSimple $ map trans l
+
+    instance QBackend SqliteBackend where
+        getBackendCode _ q = queryToSQL q
+        hQuery b@(SqliteBackend conn) q = do
+        res <- quickQuery' conn (getBackendCode b q) []
+        putStrLn $ show $ sqlValue2QueryRawRes res
+        return $ fst $ parseQueryRes $ sqlValue2QueryRawRes res
+
     
-
-
-
-
-
-
-
-
--- test.hs
+++ test.hs
@@ -7,13 +7,43 @@ data Person = Person { firstName :: String
 }
 
 
-numeros :: Entity (DBWrapper Integer)
-numeros = Entity "inteiros"
+number :: Entity Integer
+number = Entity "inteiro"
+
+numbers :: Entity [Integer]
+numbers = Entity "inteiros"
 
 main :: IO ()
-main = do 
-    putStrLn $ queryToSQL $ queryEntity numeros
-    putStrLn $ queryToSQL $ dbMap (+ (JustValue 1)) $ queryEntity numeros
+main = do
+    backend <- createSqliteBackend "test.db"
+    let f = \x -> x + toQuery (2 :: Integer)
+    
+    let q = f $ toQuery 3
+    putStrLn $ getBackendCode backend q
+    res <- hQuery backend $ q
+    putStrLn $ show res
+    
+    let q2 = queryEntity number
+    putStrLn $ getBackendCode backend q2
+    res2 <- hQuery backend $ q2
+    putStrLn $ show res2
+
+    let q3 = f q2
+    putStrLn $ getBackendCode backend q3
+    res3 <- hQuery backend $ q3
+    putStrLn $ show res3
+
+    let q4 = queryEntity numbers
+    putStrLn $ getBackendCode backend q4
+    res4 <- hQuery backend $ q4
+    putStrLn $ show res4
+    
+
+    let q5 = qmap f q4
+    putStrLn $ getBackendCode backend q5
+    res5 <- hQuery backend $ q5
+    putStrLn $ show res5
+        --putStrLn $ queryToSQL $ dbMap (+ (JustValue 1)) $ queryEntity numeros
     --                            [----------------]  [------------------]
     --                     SELECT ( (x) + 1      ) FROM  inteiros
 
